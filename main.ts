4e2198969157d0fee6b6a42f79d6dddd4307694c
ZoomZapZavier <108320919+ZoomZapZavier@users.noreply.github.com>
2022-06-28 17:02:56 +0930

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    music.playMelody("G D E F E D E F ", 225)
+    music.playMelody("A D E F E D E F ", 225)
+    music.playMelody("B D E F E D E F ", 225)
+    music.playMelody("C5 D E F E D E F ", 225)
+    music.playMelody("A D E F E D E F ", 225)
+    music.playMelody("B D E F E D E F ", 225)
+    music.playMelody("C5 D E F E D E F ", 225)
+    music.playMelody("C5 C5 C5 C5 C5 C5 B C5 ", 225)
+    music.playMelody("C5 B A G F G A C5 ", 225)
+})
